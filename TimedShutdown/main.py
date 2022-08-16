6f2fe3bf4b07d80feb903017da5242b017b5e33e
Bamboogamer <bamboogamer178@gmail.com>
2022-08-16 04:09:31 -0500

    Multiple Changes
    Added Title to Window
    Refactored to arrange Hours > Minutes > Seconds
    Refactored to remove redundant lines

--- TimedShutdown/main.py
+++ TimedShutdown/main.py
@@ -1,8 +1,10 @@
 import math
 import os
+import time
 import tkinter as tk
 from tkinter import *
 
+# Helper Method(s)
 def convertTime(num, time_str):
     time_str = time_str.lower()
     result = num
@@ -23,68 +25,59 @@ def convertTime(num, time_str):
     return result
 
 
-def createWindow():
+def timedShutdown():
+
+    # Creates Window
     win = tk.Tk()
-    win.geometry("350x300")
+    win.title('TIMED SHUTDOWN')
+    win.geometry("300x100")
+    win.eval('tk::PlaceWindow . center')
 
-    title = tk.Label(win, text="###", font="Courier 12 bold")
-    title.grid(row=0, column=1)
-    title = tk.Label(win, text="TIMED SHUTDOWN", font="Courier 12 bold")
-    title.grid(row=0, column=2)
-    title = tk.Label(win, text="###", font="Courier 12 bold")
-    title.grid(row=0, column=3)
+    # Zeros for Entry Defaults
+    zero_text1 = StringVar(win, "0")
+    zero_text2 = StringVar(win, "0")
+    zero_text3 = StringVar(win, "0")
 
-    label_Seconds = StringVar()
-    label_Seconds.set("Enter Seconds: ")
-    label_Seconds_Dir = Label(win, textvariable=label_Seconds)
-    label_Seconds_Dir.grid(row=1, column=1)
+    # Hours
+    label_Hours = StringVar(win, "Enter Hours: ")
+    label_Hours_Dir = Label(win, textvariable=label_Hours)
+    label_Hours_Dir.grid(row=1, column=1)
 
-    sec_ = Entry(win, width=30)
-    sec_.grid(row=1, column=2)
+    hr_ = Entry(win, textvariable=zero_text1, width=30)
+    hr_.grid(row=1, column=2)
 
-    label_Minutes = StringVar()
-    label_Minutes.set("Enter Minutes: ")
+    # Minutes
+    label_Minutes = StringVar(win, "Enter Minutes: ")
     label_Minutes_Dir = Label(win, textvariable=label_Minutes)
     label_Minutes_Dir.grid(row=2, column=1)
 
-    min_ = Entry(win, width=30)
+    min_ = Entry(win, textvariable=zero_text2, width=30)
     min_.grid(row=2, column=2)
 
-    label_Hours = StringVar()
-    label_Hours.set("Enter Hours: ")
-    label_Hours_Dir = Label(win, textvariable=label_Hours)
-    label_Hours_Dir.grid(row=3, column=1)
+    # Seconds
+    label_Seconds = StringVar(win, "Enter Seconds: ")
+    label_Seconds_Dir = Label(win, textvariable=label_Seconds)
+    label_Seconds_Dir.grid(row=3, column=1)
 
-    hr_ = Entry(win, width=30)
-    hr_.grid(row=3, column=2)
+    sec_ = Entry(win, textvariable=zero_text3, width=30)
+    sec_.grid(row=3, column=2)
 
     def PrintTotalSeconds():
 
-        try:
-            seconds_int = int(sec_.get())
-        except:
-            seconds_int = 0
-
-        try:
-            minutes_float = float(min_.get())
-        except:
-            minutes_float = 0
-
-        try:
-            hours_float = float(hr_.get())
-        except:
-            hours_float = 0
+        seconds_int = int(sec_.get())
+        minutes_float = float(min_.get())
+        hours_float = float(hr_.get())
 
         seconds_converted = convertTime(seconds_int, "seconds")
         minutes_converted = convertTime(minutes_float, "minutes")
         hours_converted = convertTime(hours_float, "hours")
 
-        time_in_seconds = seconds_converted + minutes_converted + hours_converted
-        print("Total Seconds:", time_in_seconds)
+        result = seconds_converted + minutes_converted + hours_converted
+        print("Total Seconds:", result)
 
         # win.destroy()
 
-    button = tk.Button(win, text="Calculate", command=PrintTotalSeconds)
+    button = tk.Button(win, text="Timed Shutdown", command=PrintTotalSeconds)
     button.grid(row=4, column=2)
 
     win.mainloop()
@@ -95,5 +88,5 @@ if __name__ == '__main__':
     # convertTime(3600.9, "seconds")
     # convertTime(60, "minutes")
     # convertTime(1.5, "hours")
-    createWindow()
+    timedShutdown()
 
